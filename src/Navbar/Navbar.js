31e11660e2ae2cdb490bf8021d3f71b826d1036a
HypnosW <hypnoswu@gmail.com>
2024-04-11 11:19:26 -0700

    still stylizing navbar

--- src/Navbar/Navbar.js
+++ src/Navbar/Navbar.js
@@ -9,14 +9,14 @@ export const Navbar = ()=>{
     return(
         <div>
             {/*Desktop Version */}
-            <div className={"d-none d-sm-none d-md-flex "
+            <div className={"d-none d-sm-none d-md-flex hw-navbar "
                             + "justify-content-between row align-items-center"}>
                 <div className={"col-lg-3 col-md-2"}>
                     <Link className={"ms-2 hw-nav-link"} to={""}>
                         Home
                     </Link>
                 </div>
-                <div className={"col-lg-6 col-md-8 d-flex justify-content-between"}>
+                <div className={"col-lg-6 col-md-8 mt-1 mb-1 d-flex justify-content-around"}>
                     <Link to={"/about"} className={"hw-nav-link"}>
                         About
                     </Link>
@@ -30,7 +30,7 @@ export const Navbar = ()=>{
                         Contact Me
                     </Link>
                 </div>
-                <div className={"col-2 col-md-2 col-lg-3  d-flex justify-content-end"}>
+                <div className={"col-2 col-md-2 col-lg-3 mt-1 mb-1 d-flex justify-content-end"}>
                     <div className={"d-flex justify-content-between"}>
                         <a className={"me-3"} href={"https://github.com/hypnosw"} target={"_blank"}>
                             <FaGithub />
