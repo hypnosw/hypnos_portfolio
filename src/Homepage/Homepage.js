b37521caf92a33285bafe8e376eecb3b02b17e76
HypnosW <hypnoswu@gmail.com>
2024-04-16 18:38:15 -0700

    finalizing title name

--- src/Homepage/Homepage.js
+++ src/Homepage/Homepage.js
@@ -1,11 +1,17 @@
 import photoshoot_bg_blur from "../Pictures/photoshoot_bg_blur.JPG";
 import "./Homepage.css"
+import "../style.css";
 
 export const Homepage = ()=>{
     return(
         <div className={""}>
           <div className={"hw-img-container"}>
-            <img className={"hw-cover-img"} alt="homePageCoverImage" src={photoshoot_bg_blur}/>
+            <img className={"hw-cover-img"} alt="homePageCoverImage"
+                 src={photoshoot_bg_blur}/>
+            <div className={"hw-cover-names-container"}>
+              <p className={"hw-cover-fname roboto-black-italic"}>Hypnos</p>
+              <p className={"hw-cover-lname roboto-black-italic"}>Wu</p>
+            </div>
           </div>
           <h1>Contents</h1>
         </div>
